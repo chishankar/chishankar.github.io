3697e6f7ec88f89cbe8c52e0068dc11deb285d24
chishankar <chishankar@gmail.com>
2017-08-11 18:57:30 -0400

    Change

--- scripts/proj_page.js
+++ scripts/proj_page.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 function SwitchToClass(classnum){
 
     if(classnum === 1) {
-        console.log("Switching to video 1");
+        console.log("Switching to class_projects");
         document.getElementById("class_projects").style.display = "block";
         document.getElementById("personal_projects").style.display = "none";
         document.getElementById("bitcamp_projects").style.display = "none";
@@ -18,7 +18,7 @@ function SwitchToClass(classnum){
         document.getElementById("word3").style.textDecoration = "none";
     }
     else if(classnum === 2) {
-        console.log("Switching to video 2");
+        console.log("Switching to personal_projects");
         document.getElementById("class_projects").style.display = "none";
         document.getElementById("personal_projects").style.display = "block";
         document.getElementById("bitcamp_projects").style.display = "none";
@@ -31,7 +31,7 @@ function SwitchToClass(classnum){
         document.getElementById("word3").style.textDecoration = "none";
     }
     else if(classnum === 3) {
-        console.log("Switching to video 3");
+        console.log("Switching to bitcamp_projects");
         document.getElementById("class_projects").style.display = "none";
         document.getElementById("personal_projects").style.display = "none";
         document.getElementById("bitcamp_projects").style.display = "block";
