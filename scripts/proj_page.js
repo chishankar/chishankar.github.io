48de1a660709f2d0bfa24adf9833570046fd6ef5
chishankar <chishankar@gmail.com>
2017-08-11 21:37:32 -0400

    Made new Changes

--- scripts/proj_page.js
+++ scripts/proj_page.js
@@ -11,26 +11,26 @@ function SwitchToClass(classnum) {
     document.getElementById("personal_projects").style.display = "none";
     document.getElementById("bitcamp_projects").style.display = "none";
 
-    document.getElementById("word1").style.textDecoration = "underline";
-    document.getElementById("word2").style.textDecoration = "none";
-    document.getElementById("word3").style.textDecoration = "none";
+    document.getElementById("word1").innerHTML= '<code>Class Projects</code>';
+    document.getElementById("word2").innerHTML= 'Personal Projects';
+    document.getElementById("word3").innerHTML= 'Bitcamp Projects';
   } else if (classnum === 2) {
     console.log("Switching to personal_projects");
     document.getElementById("class_projects").style.display = "none";
     document.getElementById("personal_projects").style.display = "block";
     document.getElementById("bitcamp_projects").style.display = "none";
 
-    document.getElementById("word1").style.textDecoration = "none";
-    document.getElementById("word2").style.textDecoration = "underline";
-    document.getElementById("word3").style.textDecoration = "none";
+    document.getElementById("word1").innerHTML= 'Class Projects';
+    document.getElementById("word2").innerHTML= '<code>Personal Projects</code>';
+    document.getElementById("word3").innerHTML= 'Bitcamp Projects';
   } else if (classnum === 3) {
     console.log("Switching to bitcamp_projects");
     document.getElementById("class_projects").style.display = "none";
     document.getElementById("personal_projects").style.display = "none";
     document.getElementById("bitcamp_projects").style.display = "block";
 
-    document.getElementById("word1").style.textDecoration = "none";
-    document.getElementById("word2").style.textDecoration = "none";
-    document.getElementById("word3").style.textDecoration = "underline";
+    document.getElementById("word1").innerHTML= 'Class Projects';
+    document.getElementById("word2").innerHTML= 'Personal Projects';
+    document.getElementById("word3").innerHTML= '<code>Bitcamp Projects</code>';
   }
 }
