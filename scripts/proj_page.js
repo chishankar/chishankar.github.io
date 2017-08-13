53a0cfccfcf546e4582ef3abbad44244b299f211
chishankar <chishankar@gmail.com>
2017-08-13 09:28:47 -0400

    Reformat

--- scripts/proj_page.js
+++ scripts/proj_page.js
@@ -11,26 +11,26 @@ function SwitchToClass(classnum) {
     document.getElementById("personal_projects").style.display = "none";
     document.getElementById("bitcamp_projects").style.display = "none";
 
-    document.getElementById("word1").innerHTML= '<b>Class Projects</b>';
-    document.getElementById("word2").innerHTML= 'Personal Projects';
-    document.getElementById("word3").innerHTML= 'Bitcamp Projects';
+    document.getElementById("word1").innerHTML= '<b>  Class  </b>';
+    document.getElementById("word2").innerHTML= ' Personal';
+    document.getElementById("word3").innerHTML= ' Bitcamp ';
   } else if (classnum === 2) {
     console.log("Switching to personal_projects");
     document.getElementById("class_projects").style.display = "none";
     document.getElementById("personal_projects").style.display = "block";
     document.getElementById("bitcamp_projects").style.display = "none";
 
-    document.getElementById("word1").innerHTML= 'Class Projects';
-    document.getElementById("word2").innerHTML= '<b>Personal Projects</b>';
-    document.getElementById("word3").innerHTML= 'Bitcamp Projects';
+    document.getElementById("word1").innerHTML= '  Class  ';
+    document.getElementById("word2").innerHTML= '<b> Personal</b>';
+    document.getElementById("word3").innerHTML= ' Bitcamp ';
   } else if (classnum === 3) {
     console.log("Switching to bitcamp_projects");
     document.getElementById("class_projects").style.display = "none";
     document.getElementById("personal_projects").style.display = "none";
     document.getElementById("bitcamp_projects").style.display = "block";
 
-    document.getElementById("word1").innerHTML= 'Class Projects';
-    document.getElementById("word2").innerHTML= 'Personal Projects';
-    document.getElementById("word3").innerHTML= '<b>Bitcamp Projects</b>';
+    document.getElementById("word1").innerHTML= '  Class  ';
+    document.getElementById("word2").innerHTML= ' Personal';
+    document.getElementById("word3").innerHTML= '<b> Bitcamp </b>';
   }
 }
