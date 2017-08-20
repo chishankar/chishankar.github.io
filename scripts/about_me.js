26e9923d8bb4dd3f26a156b9b89a964c4fc3a9de
chishankar <chishankar@gmail.com>
2017-08-20 13:26:13 -0400

    Update

--- scripts/about_me.js
+++ scripts/about_me.js
@@ -14,29 +14,17 @@ function SwitchToClass(classnum) {
     document.getElementById("current_course").style.display = "none";
     document.getElementById("school_activities").style.display = "none";
 
-    document.getElementById("button1").innerHTML= '<b>Completed Courses</b>';
-    document.getElementById("button2").innerHTML= 'Current Course Work';
-    document.getElementById("button3").innerHTML= 'School Activities';
-
   } else if (classnum === 2) {
     console.log("Switching to personal_projects");
     document.getElementById("completed_course").style.display = "none";
     document.getElementById("current_course").style.display = "block";
     document.getElementById("school_activities").style.display = "none";
 
-    document.getElementById("button1").innerHTML= 'Completed Courses';
-    document.getElementById("button2").innerHTML= '<b>Current Courses</b>';
-    document.getElementById("button3").innerHTML= 'School Activities';
-
   }else if (classnum === 3) {
     console.log("Switching to personal_projects");
     document.getElementById("completed_course").style.display = "none";
     document.getElementById("current_course").style.display = "none";
     document.getElementById("school_activities").style.display = "block";
-
-    document.getElementById("button1").innerHTML= 'Completed Courses';
-    document.getElementById("button2").innerHTML= 'Current Courses';
-    document.getElementById("button3").innerHTML= '<b>School Activities</b>';
 
   }
 }
