c87f3dbe80c12fc7e2626b7eefa036be96325772
chishankar <chishankar@gmail.com>
2017-08-22 14:14:41 -0400

    Took out the bolding

--- scripts/proj_page.js
+++ scripts/proj_page.js
@@ -11,26 +11,17 @@ function SwitchToClass(classnum) {
     document.getElementById("personal_projects").style.display = "none";
     document.getElementById("bitcamp_projects").style.display = "none";
 
-    document.getElementById("word1").innerHTML= '<b>  Class  </b>';
-    document.getElementById("word2").innerHTML= ' Personal';
-    document.getElementById("word3").innerHTML= ' Bitcamp ';
   } else if (classnum === 2) {
     console.log("Switching to personal_projects");
     document.getElementById("class_projects").style.display = "none";
     document.getElementById("personal_projects").style.display = "block";
     document.getElementById("bitcamp_projects").style.display = "none";
 
-    document.getElementById("word1").innerHTML= '  Class  ';
-    document.getElementById("word2").innerHTML= '<b> Personal</b>';
-    document.getElementById("word3").innerHTML= ' Bitcamp ';
   } else if (classnum === 3) {
     console.log("Switching to bitcamp_projects");
     document.getElementById("class_projects").style.display = "none";
     document.getElementById("personal_projects").style.display = "none";
     document.getElementById("bitcamp_projects").style.display = "block";
 
-    document.getElementById("word1").innerHTML= '  Class  ';
-    document.getElementById("word2").innerHTML= ' Personal';
-    document.getElementById("word3").innerHTML= '<b> Bitcamp </b>';
   }
 }
