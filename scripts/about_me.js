575addbfca97fa02a7553fd78c4bb29f5e0648e4
chishankar <chishankar@gmail.com>
2017-08-22 14:43:42 -0400

    Update

--- scripts/about_me.js
+++ scripts/about_me.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
   document.getElementById("completed_course").style.display = "none";
   document.getElementById("school_activities").style.display = "none";
 
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("api").style.display = "none";
+  document.getElementById("certifications").style.display = "none";
+  document.getElementById("design").style.display="none";
+
 });
 
 function SwitchToClass(classnum) {
@@ -26,4 +31,37 @@ function SwitchToClass(classnum) {
     document.getElementById("current_course").style.display = "none";
     document.getElementById("school_activities").style.display = "block";
   }
+}
+
+function SwitchToExtra(classnum) {
+
+  if (classnum === 1) {
+    console.log("Switching to languages");
+    document.getElementById("languages").style.display = "block";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("certifications").style.display = "none";
+    document.getElementById("design").style.display="none";
+
+  } else if (classnum === 2) {
+    console.log("Switching to api");
+    document.getElementById("languages").style.display = "none";
+    document.getElementById("api").style.display = "block";
+    document.getElementById("certifications").style.display = "none";
+    document.getElementById("design").style.display="none";
+
+  }else if (classnum === 3) {
+    console.log("Switching to certifications");
+    document.getElementById("languages").style.display = "none";
+    document.getElementById("api").style.display = "none";
+    document.getElementById("certifications").style.display = "block";
+    document.getElementById("design").style.display="none";
+  }
+
+}else if (classnum === 4) {
+  console.log("Switching to designs");
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("api").style.display = "none";
+  document.getElementById("certifications").style.display = "none";
+  document.getElementById("design").style.display="block";
+}
 }
